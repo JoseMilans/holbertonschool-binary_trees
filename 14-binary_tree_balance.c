dbfc476963d6f42112ab7bb2ca7d8e94361a7eb2
JoseMilans <josemilans@gmail.com>
2023-09-04 16:49:46 +0000

    Fixed: issues.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,7 +6,6 @@
  * @tree: pointer to the root node of the tree to measure the height
  * Return: the height of the tree or 0 if NULL
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
@@ -26,12 +25,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * binary_tree_balance: balance factor of tree
  * @tree: pointer to root node
  * Return: height or 0
-*/
-
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-	    return (0);
+	if (tree == NULL)
+		return (0);
 
-    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
