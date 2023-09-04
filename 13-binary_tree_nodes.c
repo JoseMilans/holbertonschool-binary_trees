ba6723ec8e3410b274d6cc40b44e9501c97ff036
JoseMilans <josemilans@gmail.com>
2023-09-04 16:42:11 +0000

    Fixed: style issues.

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,14 +5,14 @@
  * @tree: pointer to root node
  * Return: node count or 0
 */
-
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-	    return (0);
-    
-    if (tree->left != NULL || tree->right != NULL)
-	    return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
-    
-    return (0);
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return (binary_tree_nodes(tree->left) + 1 +
+				binary_tree_nodes(tree->right));
+
+	return (0);
 }
